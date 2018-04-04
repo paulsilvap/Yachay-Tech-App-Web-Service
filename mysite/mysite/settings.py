83d24d7768d21e018f67dc1becc0a2161eac3809
mau0810 <37885859+mau0810@users.noreply.github.com>
2018-04-04 04:17:11 -0500

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'events.apps.EventsConfig',
     'news.apps.NewsConfig',
     'schedule.apps.ScheduleConfig',
     'django.contrib.admin',
