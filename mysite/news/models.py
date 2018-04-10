b738ffbc9226fc3fef4a3759e760fccdfe7f68cc
pakoxtror <www.daniel_4_13@hotmail.com>
2018-04-10 16:37:03 -0500

    Campo Imagen

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 # Create your models here.
 class New(models.Model):
     title = models.CharField(max_length=200)
+    image = models.ImageField()
     pub_date = models.DateTimeField('date published')
     description = models.TextField()
     def __str__(self):
@@ -14,6 +15,8 @@ class New(models.Model):
         return self.title
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def __file__(self):
+        return self.image
 
     class Meta:
         ordering = ('pub_date',)
--- mysite/news/serializers.py
+++ mysite/news/serializers.py
@@ -7,6 +7,7 @@ import datetime
 class NewsSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=True, max_length=200)
+    image = serilizers.ImageField(required=True)
     pub_date = serializers.DateTimeField(required=True)
     description = serializers.CharField(style={'base_template': 'textarea.html'})
 
@@ -15,6 +16,7 @@ class NewsSerializer(serializers.Serializer):
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get('news_title', instance.title)
+        instance.image = validated_data.get('news_image', instance.image)
         instance.pub_date = validated_data.get('news_pub_date', instance.pub_date)
         instance.description = validated_data.get('news_description', instance.description)
         instance.save()
