d3c25706677b5fbe58dbbe357512e8314a440100
pakoxtror <www.daniel_4_13@hotmail.com>
2018-04-10 16:41:55 -0500

    campo Imagen

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -7,11 +7,7 @@ from polls.models import Question
 #  Create your models here.
 class New(models.Model):
     title = models.CharField(max_length=200)
-<<<<<<< HEAD
     image = models.ImageField()
-=======
-    #dont forget use the max length
->>>>>>> f72f869b9775e413da8be6c1cec64d2aa96c0037
     pub_date = models.DateTimeField('date published')
     description = models.TextField()
     def __str__(self):
